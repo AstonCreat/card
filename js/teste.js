0002a1a1caa1a44136d4eee584c97ded7424c33c
Italo Araujo <italoasousa@hotmail.com>
2018-04-28 17:02:14 -0300

    add logo, e adicona masi duas funcionalidades filtrar e categotia

--- js/teste.js
+++ js/teste.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
     $(".circle-intenal").hide();
+    $("#filtar").hide();
+    $("#pesquisa").hide();
+
     $(".circle-plus").click(function() {
         $(".circle-intenal").toggle(500);
     });
+
+    $("#clickFiltrar").click(function() {
+        $("#filtar").toggle(500);
+    })
+    $("#clickCategoria").click(function() {
+        $("#pesquisa").toggle(500);
+    })
 });
